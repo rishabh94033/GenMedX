e059fad6d9d793d8ac9293ad63ef883226f01a39
rishabh94033 <rishabh94033@gmail.com>
2025-03-08 00:38:06 +0530

    added email authentication

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import * as React from 'react'
+import { View, Text } from 'react-native'
+import GoogleSignIn from '@/components/auth' // Import your auth component
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to GenMedX</Text>
+      <GoogleSignIn />
     </View>
-  );
+  )
 }
